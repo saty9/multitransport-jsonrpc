f09f0c6e408b981ed6c9a3a4d6412a565721efdd
David Ellis <isv.damocles@gmail.com>
2018-08-28 23:07:47 -0700

    Remove queue-flow, migrate two files to ES6 (#11)
    * Remove queue-flow, migrate two files to ES6
    
    * Update license structure to better fit GitHub preferences
    
    * Update Travis config

--- README.md
+++ README.md
@@ -346,27 +346,3 @@ The transport is expected to have two methods: ``request`` and ``shutdown``.
 The transport is expected to have a ``shutdown`` method that behaves exactly the same as the method described above.
 
 It is also expected to make use of a ``handler`` method that the server attaches to it. This method expects two arguments, the first is a JSON-RPC object (not a string), but if the input is not valid JSON will handle the unparsed data just fine. The second argument is a callback that it provides with the response JSON-RPC object (not a string).
-
-## License (MIT)
-
-Portions Copyright (C) 2013 by Uber Technologies, Inc, David Ellis
-
-Portions Copyright (C) 2011 by Agrosica, Inc, David Ellis, Alain Rodriguez, Hector Lugo
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
--- test/client.js
+++ test/client.js
@@ -1,168 +1,167 @@
-var jsonrpc = require('../lib/index');
-var HttpTransport = jsonrpc.transports.client.http;
-var TcpTransport = jsonrpc.transports.client.tcp;
-var JSONRPCclient = jsonrpc.client;
-var shared = require('../lib/transports/shared/tcp');
-var l = require('lambda-js');
-var http = require('http');
-var net = require('net');
+var jsonrpc = require('../lib/index')
+var HttpTransport = jsonrpc.transports.client.http
+var TcpTransport = jsonrpc.transports.client.tcp
+var JSONRPCclient = jsonrpc.client
+var shared = require('../lib/transports/shared/tcp')
+var l = require('lambda-js')
+var http = require('http')
+var net = require('net')
 
 exports.loopbackHttp = function(test) {
-    test.expect(1);
-    var server = http.createServer(function(req, res) {
-        var buffer = '';
-        req.setEncoding('utf8');
-        req.on('data', function(data) {
-            buffer += data;
-        });
-        req.on('end', function() {
-            var json;
-            try {
-                json = JSON.parse(buffer);
-            } catch(e) {
-            }
-            res.write(JSON.stringify({
-                id: json && json.id,
-                result: json && json.params
-            }));
-            res.end();
-        });
-    });
-    server.listen(22222);
-    var jsonRpcClient = new JSONRPCclient(new HttpTransport('localhost', 22222));
-    jsonRpcClient.register('foo');
-    jsonRpcClient.foo('bar', function(err, result) {
-        test.equal('bar', result, 'Looped-back correctly');
-        server.close(function() {
-            test.done();
-        });
-    });
-};
+  test.expect(1)
+  var server = http.createServer(function(req, res) {
+    var buffer = ''
+    req.setEncoding('utf8')
+    req.on('data', function(data) {
+      buffer += data
+    })
+    req.on('end', function() {
+      var json
+      try {
+        json = JSON.parse(buffer)
+      } catch(e) { // eslint-disable-next-line no-empty
+      }
+      res.write(JSON.stringify({
+        id: json && json.id,
+        result: json && json.params
+      }))
+      res.end()
+    })
+  })
+  server.listen(22222)
+  var jsonRpcClient = new JSONRPCclient(new HttpTransport('localhost', 22222))
+  jsonRpcClient.register('foo')
+  jsonRpcClient.foo('bar', function(err, result) {
+    test.equal('bar', result, 'Looped-back correctly')
+    server.close(function() {
+      test.done()
+    })
+  })
+}
 
 exports.loopbackHttpWithCustomIdGenerator = function(test) {
-    test.expect(2);
-    var id = 2;
-    var generator = function() {
-        return id;
-    };
-    var server = http.createServer(function(req, res) {
-        var buffer = '';
-        req.setEncoding('utf8');
-        req.on('data', function(data) {
-            buffer += data;
-        });
+  test.expect(2)
+  var id = 2
+  var generator = function() {
+    return id
+  }
+  var server = http.createServer(function(req, res) {
+    var buffer = ''
+    req.setEncoding('utf8')
+    req.on('data', function(data) {
+      buffer += data
+    })
 
-        req.on('end', function() {
-            var json;
-            try {
-                json = JSON.parse(buffer);
-            }
-            catch(e) {
-            }
-            test.equal(json.id, id);
-            res.write(JSON.stringify({
-                id : json && json.id,
-                result: json && json.params
-            }));
-            res.end();
-        });
-    });
-    server.listen(22722);
-    var options = {
-        autoRegister: false,
-        idGenerator: generator
-    };
-    var jsonRpcClient = new JSONRPCclient(new HttpTransport('localhost', 22722), options);
-    jsonRpcClient.register('foo');
-    jsonRpcClient.foo('bar', function(err, result) {
-        test.equal('bar', result, 'Looped-back correctly');
-        server.close(function() {
-            test.done();
-        });
-    });
-};
+    req.on('end', function() {
+      var json
+      try {
+        json = JSON.parse(buffer)
+      } catch(e) { // eslint-disable-next-line no-empty
+      }
+      test.equal(json.id, id)
+      res.write(JSON.stringify({
+        id : json && json.id,
+        result: json && json.params
+      }))
+      res.end()
+    })
+  })
+  server.listen(22722)
+  var options = {
+    autoRegister: false,
+    idGenerator: generator
+  }
+  var jsonRpcClient = new JSONRPCclient(new HttpTransport('localhost', 22722), options)
+  jsonRpcClient.register('foo')
+  jsonRpcClient.foo('bar', function(err, result) {
+    test.equal('bar', result, 'Looped-back correctly')
+    server.close(function() {
+      test.done()
+    })
+  })
+}
 
 exports.loopbackHttpWithInvalidIdGenerator = function(test) {
-    test.expect(2);
-    var server = http.createServer(function(req, res) {
-        req.on('end', function() {
-            res.end();
-        });
-    });
-    server.listen(22223);
-    var generator = function() {};
-    var options = {
-        idGenerator: generator
-    };
-    var jsonRpcClient = new JSONRPCclient(new HttpTransport('localhost', 22223), options);
-    jsonRpcClient.register('foo');
-    jsonRpcClient.foo('bar', function(err) {
-        test.ok(!!err, 'error is thrown');
-        test.equals('Request id generator function should return an id', err.message);
-        server.close(function() {
-            test.done();
-        });
-    });
-};
+  test.expect(2)
+  var server = http.createServer(function(req, res) {
+    req.on('end', function() {
+      res.end()
+    })
+  })
+  server.listen(22223)
+  var generator = function() {}
+  var options = {
+    idGenerator: generator
+  }
+  var jsonRpcClient = new JSONRPCclient(new HttpTransport('localhost', 22223), options)
+  jsonRpcClient.register('foo')
+  jsonRpcClient.foo('bar', function(err) {
+    test.ok(!!err, 'error is thrown')
+    test.equals('Request id generator function should return an id', err.message)
+    server.close(function() {
+      test.done()
+    })
+  })
+}
 
 exports.failureTcp = function(test) {
-    test.expect(2);
-    var server = net.createServer(function(con) {
-        var buffers = [];
-        var bufferLen = 0;
-        var messageLen = 0;
-        con.on('data', function(data) {
-            buffers.push(data);
-            bufferLen += data.length;
-            if(messageLen === 0) messageLen = shared.getMessageLen(buffers);
-            var res, obj;
-            if(bufferLen - 4 >= messageLen) {
-                while (messageLen && bufferLen - 4 >= messageLen && (res = shared.parseBuffer(buffers, messageLen))) {
-                    buffers = res[0];
-                    obj = res[1];
-                    con.write(shared.formatMessage({
-                        id: obj && obj.id,
-                        error: "I have no idea what I'm doing."
-                    }));
-                    bufferLen = buffers.map(l('buffer', 'buffer.length')).reduce(l('fullLen, currLen', 'fullLen + currLen'), 0);
-                    messageLen = shared.getMessageLen(buffers);
-                }
-            }
-        });
-    });
-    server.listen(11111);
-    var jsonRpcClient = new JSONRPCclient(new TcpTransport({ host: 'localhost', port: 11111 }));
-    jsonRpcClient.register('foo');
-    jsonRpcClient.foo('bar', function(err) {
-        test.ok(!!err, 'error exists');
-        test.equal("I have no idea what I'm doing.", err.message, 'The error message was received correctly');
-        jsonRpcClient.transport.con.end();
-        jsonRpcClient.shutdown(function() {
-            server.close(test.done.bind(test));
-        });
-    });
-};
+  test.expect(2)
+  var server = net.createServer(function(con) {
+    var buffers = []
+    var bufferLen = 0
+    var messageLen = 0
+    con.on('data', function(data) {
+      buffers.push(data)
+      bufferLen += data.length
+      if(messageLen === 0) messageLen = shared.getMessageLen(buffers)
+      var res, obj
+      if(bufferLen - 4 >= messageLen) {
+        while (messageLen && bufferLen - 4 >= messageLen && (res = shared.parseBuffer(buffers, messageLen))) {
+          buffers = res[0]
+          obj = res[1]
+          con.write(shared.formatMessage({
+            id: obj && obj.id,
+            error: 'I have no idea what I\'m doing.'
+          }))
+          bufferLen = buffers.map(l('buffer', 'buffer.length')).reduce(l('fullLen, currLen', 'fullLen + currLen'), 0)
+          messageLen = shared.getMessageLen(buffers)
+        }
+      }
+    })
+  })
+  server.listen(11111)
+  var jsonRpcClient = new JSONRPCclient(new TcpTransport({ host: 'localhost', port: 11111 }))
+  jsonRpcClient.register('foo')
+  jsonRpcClient.foo('bar', function(err) {
+    test.ok(!!err, 'error exists')
+    test.equal('I have no idea what I\'m doing.', err.message, 'The error message was received correctly')
+    jsonRpcClient.transport.con.end()
+    jsonRpcClient.shutdown(function() {
+      server.close(test.done.bind(test))
+    })
+  })
+}
 
 exports.invalidHttp = function(test) {
-    test.expect(1);
-    var server = http.createServer(function(req, res) {
-        res.end('Hahahaha');
-    });
-    server.listen(23232);
-    var jsonRpcClient = new JSONRPCclient(new HttpTransport('localhost', 23232));
-    jsonRpcClient.register('foo');
-    jsonRpcClient.foo('bar', function(err) {
-        test.ok(err instanceof Error, 'received the error response from the client library');
-        server.close(test.done.bind(test));
-    });
-};
+  test.expect(1)
+  var server = http.createServer(function(req, res) {
+    res.end('Hahahaha')
+  })
+  server.listen(23232)
+  var jsonRpcClient = new JSONRPCclient(new HttpTransport('localhost', 23232))
+  jsonRpcClient.register('foo')
+  jsonRpcClient.foo('bar', function(err) {
+    test.ok(err instanceof Error, 'received the error response from the client library')
+    server.close(test.done.bind(test))
+  })
+}
 
 exports.serverDownHttp = function(test) {
-    test.expect(1);
-    var jsonRpcClient = new JSONRPCclient(new HttpTransport('localhost', 23232));
-    jsonRpcClient.register('foo');
-    jsonRpcClient.foo('bar', function(err) {
-        test.ok(err instanceof Error, 'received the error response from the client library');
-        test.done();
-    });
-};
+  test.expect(1)
+  var jsonRpcClient = new JSONRPCclient(new HttpTransport('localhost', 23232))
+  jsonRpcClient.register('foo')
+  jsonRpcClient.foo('bar', function(err) {
+    test.ok(err instanceof Error, 'received the error response from the client library')
+    test.done()
+  })
+}
